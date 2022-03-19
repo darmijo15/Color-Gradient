bb21d1a41ebee6afcae87a9146d394b21aca388e
darmijo15 <daniel.armijo@wsu.edu>
2022-03-19 13:11:20 -0700

    Add click-eventListener to the gradient button that generates a random hex-gradient for the background and updates the paragraph element with the assigned gradient style

---
+++ script.js
@@ -0,0 +1,23 @@
+const gradientPara = document.querySelector("#gradient-para");
+const gradientBtn = document.querySelector("#gradient-btn");
+
+gradientBtn.addEventListener('click', function() {
+    const hexValues = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
+    
+    let hex1 = `#`;
+    let hex2 = `#`;
+
+    for (let i = 0; i < 6; i++) {
+        let randomHexValue = Math.floor(Math.random() * 16);
+        hex1 += `${hexValues[randomHexValue]}`;
+    }
+
+    for (let i = 0; i < 6; i++) {
+        let randomHexValue = Math.floor(Math.random() * 16);
+        hex2 += `${hexValues[randomHexValue]}`;
+    }
+
+    console.log(hex1, hex2);
+    gradientPara.innerHTML = `background: linear-gradient(to right, ${hex1}, ${hex2})`;
+    document.body.style.background = `linear-gradient(to right, ${hex1}, ${hex2}`;
+})
